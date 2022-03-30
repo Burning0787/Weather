971bfb1ddcbdbee2f898e196be1507966383fef9
Burning0787 <86954409+Burning0787@users.noreply.github.com>
2022-03-30 19:36:14 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+const { response } = require('express');
+const API_key = 'c9e9fcaa3c3dc20fc38001043c133c5f';
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/submit', function(req,res,next){
+  const city = req.query.city;
+  const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`
+
+  axios.get(url_api)
+  .then(function (response){
+    res.render('index', {data: response.data})
+  })
+  .catch(function (error){
+    res.render(error)
+  })
+})
+
+module.exports = router;
